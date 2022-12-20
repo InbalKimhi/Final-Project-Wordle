f1aa45bacc58d4bf111c617fe7aa87b9a88a5aa4
Inbal Kimhi <inbalkm100@gmail.com>
2022-12-20 18:15:39 +0200

    added information to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,13 @@
 
 export function Home(){
     return(
+        <>
         <h1 className="d-flex justify-content-center">WELCOME USER</h1>
+        <center>
+        <p>hi ziv! im adding this text just for the assigment handin.
+        <br/>
+        Because of my progress so far, to go into the game please press on the word wordle on the top left corner, aswell as the game can only work from the virtual keyboard </p>
+        </center>
+        </>
     )
 }
