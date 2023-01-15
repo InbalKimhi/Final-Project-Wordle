21a97492a5d7e54dac9e3d751afb8bebef585f95
Inbal Kimhi <inbalkm100@gmail.com>
2023-01-15 16:06:13 +0200

    saving testing files for later use

---
+++ src/comp/keyBoard.cy.js
@@ -0,0 +1,9 @@
+import { expect } from "chai"
+import { KeyBoard } from "./KeyBoard"
+describe('', ()=>{
+    it('keyBoard testing', () => {
+        // cy.mount(<KeyBoard/>)
+        // cy.get('button').first().should('have.text','Q')
+        expect(true).to.be.equal(true)
+      })
+})
